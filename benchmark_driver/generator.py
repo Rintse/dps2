ad82c6595cfcfc3332c1d8acbd5aed6f03c42689
Majeux <majeux@pop-os.localdomain>
2020-11-19 16:25:07 +0100

    generate nu votes

--- benchmark_driver/generator.py
+++ benchmark_driver/generator.py
@@ -6,60 +6,61 @@ from time import sleep
 from queue import Full as queueFullError
 import ntplib
 from numpy.random import normal
-
-# .py
-from our_ntp import getLocalTime
-from streamer import STOP_TOKEN # To be put in an error queue in case a Generator encounters an exception.
+from time import time
 
 # Data ranges for Tuple Generator.
 GEM_RANGE   = 100
 PRICE_RANGE = 5
 
+STOP_TOKEN = "_STOP_"
+
 # Queue parameters
 PUT_TIMEOUT = 0
+
+def county_list():
+    return open("../counties.dat", "r").read().splitlines()
 
 # Generates a random number from a normal distribution in range [lower_bound, upper_bound).
 def rand_normal(mean, sd, lower_bound, upper_bound):
     return min( max( int( round( normal(mean, sd) ) ), 0), upper_bound-1)
 
-# Tuple generator for a purchase-tuple = (GemId, price, event_time)
-# Will generate the time stamp with NTP if a time_client is available, else it uses time.time()
-def gen_purchase(time_client=None):
-    purchase = (
-        rand_normal((GEM_RANGE-1)/2, GEM_RANGE/4, 0, GEM_RANGE),
-        randrange(PRICE_RANGE),
-        getLocalTime(time_client)
-    )
-    return purchase
+# gets a random element from list
+def rand_from(target_list, size):
+    return target_list[randrange(size)]
 
-# Generator that performs calls to purchase tuple generator and fill the shared queue `q`.
-# `budget` function calls are made at a rate of `rate` per second.
-# If it can not put data in the `q`, it throws an exception and communicates this through the 
-# shared `error` queue
-def purchase_generator(q, error, time_client, id, rate, budget):
-    print("Start purchase generator ", id)
+# generate a random vote
+def gen_vote(counties, n):
+    return (rand_from(counties, n), randrange(2), time())
+
+def vote_generator(q, error, time_client, id, rate, budget):
+    print("Start vote generator ", id)
+
+    counties = county_list()
+    n = len(counties)
 
     for _ in range(budget):
-        start = getLocalTime(time_client)
+        start = time()
 
         try:
-            q.put(gen_purchase(time_client), PUT_TIMEOUT)
+            q.put(gen_vote(counties, n), PUT_TIMEOUT)
         except queueFullError as e:
             error.put(STOP_TOKEN)
             raise RuntimeError("\n\tGenerator-{} reached Queue threshold\n".format(id)) from e
 
-
-        diff = getLocalTime(time_client) - start
+        diff = time() - start
         sleep(max(0, 1/rate - diff))
 
 # Small test of generator functionality
 if __name__ == "__main__":
     print("__________________\nTest ad_generator")
 
-    # for i in range(10):
-    #     print(gen_purchase())
+    counties = county_list()
+    n = len(counties)
 
-    res = [rand_normal((GEM_RANGE-1)/2, GEM_RANGE/4, 0, GEM_RANGE) for i in range(10000)]
-    dist = [res.count(i) for i in range(GEM_RANGE)]
-    print(dist)
+    for i in range(10):
+        print(gen_vote(counties, n))
+
+    # res = [rand_normal((GEM_RANGE-1)/2, GEM_RANGE/4, 0, GEM_RANGE) for i in range(10000)]
+    # dist = [res.count(i) for i in range(GEM_RANGE)]
+    # print(dist)
 
