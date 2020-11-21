0f8d6d12eac7ed46e826d937a9ddeb05ef326471
rintse <r1ntse.a@gmail.com>
2020-11-21 13:31:15 +0100

    target cluster sowieso een goed argument

--- deployment/remote_deploy.py
+++ deployment/remote_deploy.py
@@ -4,24 +4,16 @@ import sys
 import subprocess
 
 if len(sys.argv) < 3:
-    print("Must supply num_workers and gen_rate")
+    print("Must supply num_workers, gen_rate and target cluster")
     exit()
 
 # Syncs project folder with DAS5, before running the cluster
 if os.getcwd().split("/")[-1] != "dps2":
     print("This script must be called from the project root dir")
 else:
-    if len(sys.argv) == 3:
-        print("Copying changes to the das... ", end='')
-        os.system("rsync -r --delete . das5:DPS2")
-        print("done.")
+    print("Copying changes to the das... ", end='')
+    os.system("rsync -r --delete . "  + sys.argv[3] + ":DPS2")
+    print("done.")
 
-        print("Running remotely:")
-        os.system("ssh das5 DPS2/deployment/run_bench.py " + sys.argv[1] + " " + sys.argv[2])
-    else:
-        print("Copying changes to the das... ", end='')
-        os.system("rsync -r --delete . "  + sys.argv[3] + ":DPS2")
-        print("done.")
-
-        print("Running remotely:")
-        os.system("ssh " + sys.argv[3] + " DPS2/deployment/run_bench.py " + sys.argv[1] + " " + sys.argv[2])
+    print("Running remotely:")
+    os.system("ssh " + sys.argv[3] + " DPS2/deployment/run_bench.py " + sys.argv[1] + " " + sys.argv[2])
