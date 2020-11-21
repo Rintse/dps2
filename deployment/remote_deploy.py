d9e3f185ca1de090bee2cb74c56ef5688d9fd98f
Majeux <majeux@pop-os.localdomain>
2020-11-21 13:17:33 +0100

    optioneel ssh target argument, default is nog steeds jouw alias

--- deployment/remote_deploy.py
+++ deployment/remote_deploy.py
@@ -11,9 +11,17 @@ if len(sys.argv) < 3:
 if os.getcwd().split("/")[-1] != "dps2":
     print("This script must be called from the project root dir")
 else:
-    print("Copying changes to the das... ", end='')
-    os.system("rsync -r --delete . das5:DPS2")
-    print("done.")
+    if len(sys.argv) == 3:
+        print("Copying changes to the das... ", end='')
+        os.system("rsync -r --delete . das5:DPS2")
+        print("done.")
 
-    print("Running remotely:")
-    os.system("ssh das5 DPS2/deployment/run_bench.py " + sys.argv[1] + " " + sys.argv[2])
+        print("Running remotely:")
+        os.system("ssh das5 DPS2/deployment/run_bench.py " + sys.argv[1] + " " + sys.argv[2])
+    else:
+        print("Copying changes to the das... ", end='')
+        os.system("rsync -r --delete . "  + sys.argv[3] + ":DPS2")
+        print("done.")
+
+        print("Running remotely:")
+        os.system("ssh " + sys.argv[3] + " DPS2/deployment/run_bench.py " + sys.argv[1] + " " + sys.argv[2])
